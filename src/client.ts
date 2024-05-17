8b7eaf02b4bf7c0896e76e8bcb4a439224560fcf
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2024-05-17 15:54:45 +0200

    clean comment

--- src/client.ts
+++ src/client.ts
@@ -2,20 +2,5 @@ import { Socket, createConnection } from "net";
 import { Client } from "./helper/client";
 import { config } from "./config/config";
 
-// 
-
-// console.log('Public Key:', keys.publicKey);
-// console.log('Private Key:', keys.privateKey);
-
-// const message = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae expedita consectetur veritatis earum nulla quisquam, facere vero illo aliquam enim corporis, aliquid, vitae dolor architecto sapiente animi. Quaerat laboriosam reiciendis aspernatur cupiditate illo. Magnam, ad laudantium! Minima laboriosam adipisci voluptatem commodi quaerat eum aut culpa ut aliquid similique officiis hic voluptas nisi modi ipsam ipsa, et velit dolorem itaque! Amet eligendi voluptas corporis provident unde nobis quas excepturi magni libero iusto dolore nemo, ipsam facere laudantium iste eveniet officiis nostrum temporibus quo dolorum eum nam ducimus laborum dicta. Similique blanditiis cupiditate itaque aliquid minus placeat. Error eos repellat tempora a!"
-// console.log("Claire: ", message)
-
-// console.log("Chiffrer: ", ciphedMessage)
-
-// const clearMessage = decrypt(ciphedMessage, keys.privateKey)
-// console.log("Dechiffrer: ", clearMessage)
-
-
 const HOST = '127.0.0.1';
-
 const client = new Client(HOST, config.serverPort)
